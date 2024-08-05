5ed64f88cd80e631187091b742bed798a562b5e8
samuellangajr <samuellangajr@gmail.com>
2024-08-05 21:34:37 +0200

    feat: responsive amount price

--- frontend/src/components/sendMoney/amount.tsx
+++ frontend/src/components/sendMoney/amount.tsx
@@ -22,13 +22,13 @@ const Amount: React.FC = () => {
                     className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none sm:text-sm"
                 />
             </div>
-            <div className="mt-4 grid grid-cols-4 gap-4">
+            <div className="mt-4 grid grid-cols-4 gap-3">
                 {amounts.map((value) => (
                     <button
                         key={value}
                         type="button"
                         onClick={() => handleButtonClick(value)}
-                        className="py-2 px-3 border border-gray-300 rounded-md text-center focus:outline-none focus:ring-1 focus:ring-mpesa focus:border-mpesa hover:border-mpesa sm:text-sm"
+                        className="p-2 border border-gray-300 rounded-md text-center focus:outline-none focus:ring-1 focus:ring-mpesa focus:border-mpesa hover:border-mpesa text-xs sm:text-sm"
                     >
                         {value} MT
                     </button>
